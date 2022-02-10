fb7c66e1182cec749a8c55c69bca143328f45b63
Saif Mustaf <43801473+iamsaifmustaf@users.noreply.github.com>
2022-02-10 16:46:11 -0500

    counting valleys challenge

--- sock_sorter.py
+++ sock_sorter.py
@@ -4,13 +4,28 @@ import random
 import re
 import sys
 
-# Complete the 'sockMerchant' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER_ARRAY ar
-#
+# There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+# Example
+# There is one pair of color and one of color . There are three odd socks left, one of each color. The number of pairs is
+
+# Function Description
+# Complete the sockMerchant function in the editor below.
+# sockMerchant has the following parameter(s):
+
+#     int n: the number of socks in the pile
+#     int ar[n]: the colors of each sock
+
+# Returns
+
+#     int: the number of pairs
+
+# Input Format
+
+# The first line contains an integer
+# , the number of socks represented in .
+# The second line contains space-separated integers, , the colors of the socks in the pile.
+
 def sockMerchant(n, ar):
     sock_dictionary = {}
     total_pairs = 0
@@ -23,5 +38,7 @@ def sockMerchant(n, ar):
             total_pairs += sock_dictionary[key] // 2
 
     return total_pairs
+
+
 
 print(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]))
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-class Tests(unittest.TestCase):
+# class Tests(unittest.TestCase):
 
-    def test_sock_sorter(self):
-        self.assertEqual(sockMerchant(n, ar))
+#     def test_sock_sorter(self):
+#         self.assertEqual(sockMerchant(n, ar))
